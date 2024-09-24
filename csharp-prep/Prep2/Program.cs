f43a0aaba933d706357c0bc96d75b1889b57ea99
Emberhand <haydbill@gmail.com>
2024-09-23 19:00:18 -0600

    C# Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string userGrade = Console.ReadLine();
+
+        int grade = int.Parse(userGrade);
+        string letterGrade = "";
+        string status = "";
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+            status = "passed";
+        }
+        else if (grade < 90 && grade >= 80)
+        {
+            letterGrade = "B";
+            status = "passed";
+        }
+         else if (grade < 80 && grade >= 70)
+        {
+            letterGrade = "C";
+            status = "passed";
+        }
+         else if (grade < 70 && grade >= 60)
+        {
+            letterGrade = "D";
+            status = "failed";
+        }
+         else if (grade < 60)
+        {
+            letterGrade = "F";
+            status = "failed";
+        }
+
+        Console.WriteLine($"{letterGrade}");
+        Console.WriteLine("");
+
+        if (status == "passed")
+        {
+            Console.WriteLine("You passed the class! Congratulations!");
+        }
+        else
+        {
+            Console.WriteLine("You failed the class. Keep trying!");
+        }
     }
 }
