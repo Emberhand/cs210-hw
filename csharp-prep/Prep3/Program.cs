b2f3f7e5403fdbf77563b46c9badd261f098c4e3
Emberhand <haydbill@gmail.com>
2024-09-24 18:43:11 -0600

    C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,100);
+
+        Console.WriteLine($"{number}");
+        int guess = 0;
+        
+        while (!(number == guess))
+        {
+            Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess == number) {
+                Console.WriteLine("Yep, you guessed it!");
+            }
+            else if (guess > number) {
+                Console.WriteLine("Lower.");
+            }
+            else {
+                Console.WriteLine("Higher.");
+            }
+        }
     }
+
 }
