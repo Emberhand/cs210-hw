c1b696fa5e792af24956c1fb383565a21590a1c7
Emberhand <haydbill@gmail.com>
2024-09-25 18:39:06 -0600

    Learning 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Oracle";
+        job1._startYear = 2002;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Optics Installer";
+        job2._company = "E2 Optics";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+        Resume resume1 = new Resume();
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1._name = "Hayden Billings";
+
+        resume1.DisplayResume();
     }
 }
